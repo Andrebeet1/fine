418746fc8e1f62c670e72faf3786cd258438a8b8
andre <andrebeet1@gmail.com>
2025-05-07 22:32:51 +0200

    Update settings.py

--- church_finance/settings.py
+++ church_finance/settings.py
@@ -70,6 +70,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'   # Obligatoire pour Render collectstatic
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+
 
