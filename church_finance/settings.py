8c85ad74c21b39d6c3ae580b9b9c06c7fec824bc
andre <andrebeet1@gmail.com>
2025-05-07 21:06:48 +0200

    Update settings.py

--- church_finance/settings.py
+++ church_finance/settings.py
@@ -6,8 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-your-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['gestion-finances-eglise.onrender.com', '127.0.0.1']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
